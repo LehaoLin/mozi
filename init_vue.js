273a87c1c496e92480a67b612de97cc95aca1935
LehaoLin <lehaolin@link.cuhk.edu.cn>
2023-11-29 11:35:17 +0800

    update

--- init_vue.js
+++ init_vue.js
@@ -4,7 +4,7 @@ const exists = await fs.pathExists("./frontend");
 if (exists) {
   echo("The frontend dir already exists? Please delete it.");
 } else {
-  await $`pnpm create vite frontend --template vue`;
+  await $`npx degit LehaoLin/frontend frontend`;
   cd("./frontend");
   await $`pnpm i`;
   await $`pnpm build`;
